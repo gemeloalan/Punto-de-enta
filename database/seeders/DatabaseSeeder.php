16f3d2d00a67e0dd863268ca2d62fbb441bab1e6
Alan <mago7410alan@gmail.com>
2022-12-01 07:28:17 -0600

    aiudaaa

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,7 +29,7 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
-        // Vamos bien
+        // Vamos bien uwu
        
          State::factory(15)->create();
          Municipality::factory(15)->create();
